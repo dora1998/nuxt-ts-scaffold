822ce981b77881ccd56a7f50d6ea6fd011253ec7
dora1998 <dorayaki.app@gmail.com>
2019-04-09 02:27:04 +0900

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    ecmaFeatures: { "legacyDecorators": true }
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+    'prettier/vue',
+    'prettier/@typescript-eslint'
+  ],
+  plugins: [
+    'prettier',
+    '@typescript-eslint'
+  ],
+  // add your custom rules here
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error"
+  }
+}
